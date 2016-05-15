542df79ce0a75f0397d9d4f95deca68e55aea6a9
Andrew <avpk77@gmail.com>
2016-05-15 15:13:51 +0300

    Srart work with prog

---
+++ src/main/java/com.clack.threemonthjunior.starter/FileHandling/FileMaker.java
@@ -0,0 +1,60 @@
+package Compare.FileHandling;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by a_pan on 15.05.2016.
+ */
+public class FileMaker {
+    private File newFileVersion;
+    private File oldFileVersion;
+    private File deltaFile;
+
+    public void accsesToFiles(String path) {
+
+        try {
+            newFileVersion = getAccessFile(path + System.getProperty("file.separator") + "test_new.txt");
+            oldFileVersion = getAccessFile(path + System.getProperty("file.separator") + "test_old.txt");
+            deltaFile = getAccesDeltaFile(path + System.getProperty("file.separator") + "tmp.txt");
+            Writer.setNameDeltaFile(path + System.getProperty("file.separator") + "tmp.txt");
+            Writer.setNameNewFile(path + System.getProperty("file.separator") + "test_new.txt");
+            Writer.setNameOldFile(path + System.getProperty("file.separator") + "test_old.txt");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+
+    }
+
+    private File getAccessFile(String way) {
+        File unit = new File(way);
+        return unit;
+    }
+    private File getAccesDeltaFile(String way) {
+        File unit = new File(way);
+        if (unit.exists()) {
+            try {
+                unit.delete();
+                unit.createNewFile();
+            } catch (IOException ex){
+                ex.printStackTrace();
+            }
+
+        }
+        return unit;
+    }
+
+    public File getNewFileVersion() {
+        return newFileVersion;
+    }
+
+    public File getOldFileVersion() {
+        return oldFileVersion;
+    }
+
+    public File getDeltaFile() {
+        return deltaFile;
+    }
+}
---
+++ src/main/java/com.clack.threemonthjunior.starter/Test.java
@@ -0,0 +1,7 @@
+package Compare;
+
+/**
+ * Created by a_pan on 13.05.2016.
+ */
+public class Test {
+}
